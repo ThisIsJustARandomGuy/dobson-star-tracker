b50440d91b0f52d2ac458f668522ca398f6b316c
Hofer Lukas <hoferlukas1@gmail.com>
2019-11-03 19:57:24 +0100

    Remove unnecessary files

--- Debug/eeprom.cpp.d
+++
@@ -1,2 +0,0 @@
-C:\Users\lukas\AppData\Local\Temp\VMBuilds\dobson-star-tracker\mega_atmega2560\Debug\eeprom.cpp.o: \
- C:\Users\lukas\AppData\Local\Temp\VMBuilds\dobson-star-tracker\mega_atmega2560\Debug\eeprom.cpp
